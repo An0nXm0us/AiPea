34b35141c28b02cb65a5d507e80b2304b5fa08c4
unknown <m.menyatsoe@gmail.com>
2025-08-04 21:21:17 +0200

    Updatedfunctionality.

---
+++ Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Eddie.Models
+{
+    //This is an extension of the user details.
+    public class Users: IdentityUser
+    {
+        [Required]
+        public string FirstName {  get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public string Role {  get; set; }
+
+
+        [Required]
+        public string Institution { get; set; }
+    }
+}
