e4e4fbefb429feb9b6d234b68039faa95fb165e3
mvelo8 <st10397913@vcconnect.edu.za>
2025-09-24 18:01:59 +0200

    add

--- app/src/main/java/vcmsa/projects/wilproject/LoginEvent.kt
+++
@@ -1,7 +0,0 @@
-package vcmsa.projects.wilproject
-
-sealed interface LoginEvent {
-    data class checkUsername(val username: String) : LoginEvent
-    data class checkPassword(val password: String) : LoginEvent
-    object Login : LoginEvent
-}
--- app/src/main/java/vcmsa/projects/wilproject/LoginState.kt
+++
@@ -1,9 +0,0 @@
-package vcmsa.projects.wilproject
-
-data class LoginState(
-    val username: String = "",
-    val password: String = "",
-    val isLoading: Boolean = false,
-    val isSuccess: Boolean = false,
-    val errorMessage: String? = null
-)
